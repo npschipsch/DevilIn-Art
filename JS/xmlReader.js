50bfa040504f77d253fbe2b0915b15853f47735a
kostoele <kosto3530010>
2019-01-25 18:07:04 +0100

    XML (store)

--- JS/xmlReader.js
+++ JS/xmlReader.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function (evt) {
     // read the XML content and when ready, display
     // the XML data by invoking the showXMLData function
     loadXML();
+    loadXML2();
 });
 
 function loadXML() {
@@ -13,6 +14,23 @@ function loadXML() {
     };
     xhttpreq.open("GET", "../XMLtables/commissionsTable.xml", true);
     xhttpreq.send();
+
+
+}
+
+function loadXML2() {
+    var xhttpreq2 = new XMLHttpRequest();
+    xhttpreq2.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            createXMLTable2(this);
+        }
+    };
+    xhttpreq2.open("GET", "./XMLtables/storeTable.xml", true);
+    xhttpreq2.send();
+    ;
+
+
 }
 
 function createXMLTable(xml) {
@@ -32,4 +50,33 @@ function createXMLTable(xml) {
 
     document.getElementById("table").innerHTML = table;
     document.getElementById("table2").innerHTML = table;
+}
+
+function createXMLTable2(xml2) {
+    var i;
+    var xmlTable = xml2.responseXML;
+    var table1 = "<tr style='background-color: #94618e;height: 50px;'><th>Artist</th><th>T-Shirt</th><th>Mug</th><th>Wallpaper</th></tr>";
+    var table2 = "<tr style='background-color: #94618e;height: 50px;'><th>Artist</th><th>T-Shirt</th><th>Mug</th><th>Wallpaper</th></tr>";
+
+    var x = xmlTable.getElementsByTagName("store");
+    var y = xmlTable.getElementsByTagName("store2");
+    for (i = 0; i < x.length; i++) {
+        table1 += "<tr><td>" +
+            x[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue + "</td><td>" +
+            x[i].getElementsByTagName("tShirt")[0].childNodes[0].nodeValue + "</td><td>" +
+            x[i].getElementsByTagName("mug")[0].childNodes[0].nodeValue + "</td><td>" +
+            x[i].getElementsByTagName("wallpaper")[0].childNodes[0].nodeValue + "</td>";
+
+    }
+
+    document.getElementById("store").innerHTML = table1;
+    for (i = 0; i < y.length; i++) {
+        table2 += "<tr><td>" +
+            y[i].getElementsByTagName("artist2")[0].childNodes[0].nodeValue + "</td><td>" +
+            y[i].getElementsByTagName("tShirt2")[0].childNodes[0].nodeValue + "</td><td>" +
+            y[i].getElementsByTagName("mug2")[0].childNodes[0].nodeValue + "</td><td>" +
+            y[i].getElementsByTagName("wallpaper2")[0].childNodes[0].nodeValue + "</td>";
+
+    }
+    document.getElementById("store2").innerHTML = table2;
 }
